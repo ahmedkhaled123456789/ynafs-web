b014cc3f1f8a4c26a3e5980e07d8052051ad5018
MahmoudY3c <wpsmahmoud25@gmail.com>
2025-07-23 01:57:32 +0300

    feat: show all book + add meta tags

--- src/pages/SubjectsPage.jsx
+++ src/pages/SubjectsPage.jsx
@@ -1,10 +1,10 @@
-import { useEffect } from "react"; 
-import { FaBook ,FaDownload} from "react-icons/fa";
+import { useEffect } from "react";
+import { FaBook, FaDownload } from "react-icons/fa";
 import { Link, useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getSubjects } from "../store/categoriesSlice";
 import { baseURL } from "../Api/baseURL";
- const useQuery = () => new URLSearchParams(useLocation().search);
+const useQuery = () => new URLSearchParams(useLocation().search);
 
 const SubjectsPage = () => {
   const query = useQuery();
@@ -20,7 +20,10 @@ const SubjectsPage = () => {
   }, [dispatch, semesterId]);
 
   return (
-    <div dir="rtl" className="min-h-screen bg-gray-100 flex flex-col items-center p-12">
+    <div
+      dir="rtl"
+      className="min-h-screen bg-gray-100 flex flex-col items-center p-12"
+    >
       <h1 className="text-3xl font-bold mb-8">المواد الدراسية</h1>
 
       {loading && <p className="text-gray-500">جاري التحميل...</p>}
@@ -32,25 +35,37 @@ const SubjectsPage = () => {
             key={subject._id || index}
             className="bg-white rounded-2xl shadow-md p-6 flex flex-col items-center justify-center "
           >
-            <div className="mb-4"><FaBook size={70} /></div>
-            <div className="text-lg font-semibold text-center mb-4">{subject.title}</div>
-           {subject.books && subject.books.length > 0 ? (
-  <a
-    href={`${(import.meta.env.DEV ? baseURL : "") + subject.books[0]?.path}`}
-    download={subject.books[0]?.title + ".pdf"}
-    className="mt-4 inline-flex items-center gap-2 bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-800 transition"
-  >
-    <FaDownload />
-    تحميل الكتاب
-  </a>
-) : (
-  <p className="mt-4 text-red-600 font-medium">الكتاب غير متاح الآن</p>
-)}
-
-    
-
-
-           
+            <div className="mb-4">
+              <FaBook size={70} />
+            </div>
+            <div className="text-lg font-semibold text-center mb-4">
+              {subject.title}
+            </div>
+            {subject.books && subject.books.length > 0 ? (
+              subject.books.map((e) => {
+                return e.title.includes("غلاف ") ? null : (
+                  <a
+                    key={e.path}
+                    href={
+                      !e?.path?.startsWith("http")
+                        ? (import.meta.env.DEV ? baseURL : "") + e?.path
+                        : e?.path
+                    }
+                    download={e?.title + ".pdf"}
+                    className="mt-4 inline-flex items-center justify-center gap-2 bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-800 transition"
+                  >
+                    <FaDownload />
+                    <span className="text-center">
+                      {e.title.split("/")[1]?.trim() || e.title}
+                    </span>
+                  </a>
+                );
+              })
+            ) : (
+              <p className="mt-4 text-red-600 font-medium">
+                الكتاب غير متاح الآن
+              </p>
+            )}
           </div>
         ))}
       </div>
