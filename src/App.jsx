475ed1c3896705a004da4770f3754911c1793b9a
MahmoudY3c <wpsmahmoud25@gmail.com>
2025-07-25 15:12:04 +0300

    chore: move users to main page

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,24 @@ import { useEffect } from "react";
 import { setBreadcrumbPath } from "./store/categoriesSlice";
 import { useDispatch } from "react-redux";
 import InitDataLoader from "./handlers/InitDataLoader";
+import { baseURL } from "./Api/axiosRequest";
 
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
     const savedPath = localStorage.getItem("breadcrumbPath");
+    const isComingFromHomePage = localStorage.getItem("isComingFromHomePage");
+
     if (savedPath) {
       dispatch(setBreadcrumbPath(JSON.parse(savedPath)));
     }
+
+    // if user not coming from main page then move them back to home page
+    if (import.meta.env.PROD && !isComingFromHomePage) {
+      location.href = `${baseURL}/#stages`;
+    }
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
