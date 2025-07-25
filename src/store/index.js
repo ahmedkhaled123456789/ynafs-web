718d9952ca6878c90fe76b98bfb3bde6e5e8c5d7
MahmoudY3c <wpsmahmoud25@gmail.com>
2025-07-25 21:03:49 +0300

    chore: enable redux devtools in dev mode

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categoriesReducer from "./categoriesSlice";
 const store = configureStore({
+  devTools: import.meta.env.DEV,
   reducer: {
     category: categoriesReducer,
   },
